75effea60200f57f186c455706be2fb4a715a1ec
liakassardjian <liakassardjian@gmail.com>
2020-03-31 16:27:58 -0300

    Criação do projeto

---
+++ LearningSplitView-SwiftUI/LearningSplitView-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearningSplitView-SwiftUI
+//
+//  Created by Lia Kassardjian on 31/03/20.
+//  Copyright © 2020 Lia Kassardjian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
